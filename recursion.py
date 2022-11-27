7b8cae7724870fe675a4197719d83ab6d4d8f25b
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-11-27 15:50:40 +0300

    Create recursion.py

---
+++ recursion.py
@@ -0,0 +1,10 @@
+def main():
+    # Your code here
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(1 << 30)
+    threading.stack_size(1 << 27)
+
+    main_thread = threading.Thread(target=main)
+    main_thread.start()
+    main_thread.join()
